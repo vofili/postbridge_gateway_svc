3c4a249596fd323c332051cff93e45f5dd0aa46d
support@Nubeero.com <support@nubeero.com>
2023-07-03 04:22:25 +0100

    rebuild project- modified FundsTransferSinkProcessor

--- tms-switch-service/src/main/java/com/tms/service/TmsSwitchApplication.java
+++ tms-switch-service/src/main/java/com/tms/service/TmsSwitchApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TmsSwitchApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TmsSwitchApplication.class, args);
     }
